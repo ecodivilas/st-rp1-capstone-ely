95fb4cf6abed3d6ac5a7d06c4e5baf2945726163
ely <eleazar.odivilas01@gmail.com>
2023-07-08 22:12:25 +0800

    feature: created controller folder

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+const userController = require('./controller/users')
 
 const app = express()
 const port = process.env.DB_PORT || 3000
 
-console.log('WHAT IS THE PORT', port)
+app.use(express.static(path.join(__dirname, './ui/build/')))
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to Awesome Application!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Server listening on the port: ${port}`)
 })
