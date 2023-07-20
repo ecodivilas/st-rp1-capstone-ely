0d18d18abc654a96c765a368d2c2bcaf67cfe8d5
omi-elyodivilas <eleazar.odivilas@smsupermalls.com>
2023-07-20 20:48:11 +0800

    feature: using pg admin to fetch data from the database

--- server/server.js
+++ server/server.js
@@ -3,14 +3,12 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const path = require('path')
 
-const userController = require('./controller/users')
+const pool = require('./config/db')
 
 const app = express()
-const port = process.env.DB_PORT || 3000
+const port = process.env.SERVER_PORT || 3000
 
-app.use(express.static(path.join(__dirname, './ui/build/')))
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
@@ -18,6 +16,19 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
     res.send('Welcome to Awesome Application!')
 })
+
+
+app.get("/users", async(req, res) => {
+    try {
+        const users = await pool.query("SELECT * FROM users");
+        res.json(users.rows);
+    
+    } catch (err) {
+        console.error(err.message);
+        
+    }
+    
+});
 
 app.listen(port, () => {
     console.log(`Server listening on the port: ${port}`)
